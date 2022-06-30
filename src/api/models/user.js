416aafc02b3c4b209be5c671c12479694484e5bb
SamuelDAlencar <sarl.2001@hotmail.com>
2022-06-30 11:53:53 -0300

    feat: added the prestart script to the package.json

---
+++ src/api/models/user.js
@@ -0,0 +1,12 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    username: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+
+  return User;
+};
--- src/redux/slices/userSlice.js
+++ src/redux/slices/userSlice.js
@@ -1,13 +1,16 @@
-import { createSlice } from "@reduxjs/toolkit";
+// import { createSlice } from "@reduxjs/toolkit";
+// import { User } from '../../api/models';
 
-const userSlice = createSlice({
-  name: 'user',
-  initialState: [],
-  reducers: {
-    signUp(state, action) {
-      const userExists = action.payload.email === models.
+// // const userSlice = createSlice({
+// //   name: 'user',
+// //   initialState: [],
+// //   reducers: {
+// //     async signUp(state, action) {
+// //       const userExists = await User.findOne({ where: { email: action.payload.email } });
 
-      state.push
-    }
-  }
-});
+// //       if (userExists === null) {
+
+// //       }
+// //     }
+// //   }
+// // });
