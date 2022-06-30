e1d9f19a639acb2eae981a2101200fdac6257a17
SamuelDAlencar <sarl.2001@hotmail.com>
2022-06-30 11:06:51 -0300

    feat: added the logIn page and the signUp page (still not working and with missing dependencies)

--- src/App.js
+++ src/App.js
@@ -1,48 +1,18 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import { addTask } from './redux/slices/tasksSlice';
-import './style/App.css';
-import Tasks from './components/Task';
+import React from 'react';
+import Home from './pages/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
 
 function App() {
-  const dispatch = useDispatch();
-  const [currTask, setCurrTask] = useState([]);
-  const [taskId, setTaskId] = useState(1);
-
-  const handleChange = (event) => {
-    setCurrTask(event.target.value);
-  };
-
-  const handleAddTask = () => {
-    dispatch(addTask({ id: taskId, subject: currTask }));
-    setTaskId(taskId + 1);
-  };
-
   return (
-    <>
-      <Header />
-      <main className='app_main'>
-        <section className="addTask_section">
-          <h3 className="main_h3">New Task</h3>
-          <input onChange={handleChange} />
-          <button
-            onClick={() => handleAddTask()}
-          >Add task</button>
-        </section>
-        <section className="tasks_section">
-          <section className="ongoingTasks_section">
-            <h4>Ongoing</h4>
-            <Tasks />
-          </section>
-          <section className="doneTasks_section">
-            <h4>Done</h4>
-          </section>
-        </section>
-      </main>
-      <Footer />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/signup' element={ <SignUp /> } />
+        <Route exact path='/' element={ <Login /> } />
+        <Route exact path='home' element={ <Home /> } />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Login.js
@@ -0,0 +1,216 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { loginAction } from '../redux/actions';
+import { MIN_PASS_LENGTH } from '../consts';
+import {
+  Button,
+  TextField,
+  Typography,
+  ThemeProvider,
+  createTheme,
+  Container,
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import '../css/Login.css';
+
+// This Login page and its style was taken from a personal project (available on my GitHub as "koala_project");
+
+function Login() {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#00ADB5'
+      }
+    }
+  });
+
+  const useStyles = makeStyles({
+    title: {
+      color: '#EEEEEE',
+      margin: '50px 0',
+    },
+
+    inputSection: {
+      display: 'flex',
+      width: '90%',
+      maxWidth: '500px',
+      justifyContent: 'center',
+      alignItems: 'center',
+      margin: '15px 0 0 0',
+    },
+
+    input: {
+      width: '100%',
+      color: '#EEEEEE',
+      fontSize: '100%',
+    },
+
+    buttonSection: {
+    },
+
+    buttons: {
+      width: '60%',
+      maxWidth: '300px',
+      fontSize: '85%',
+      padding: '15px',
+      marginTop: '20px',
+    },
+
+    container: {
+      borderRadius: '0',
+      height: '100vh',
+      width: '100vw',
+      backgroundColor: '#222831',
+      color: '#EEEEEE',
+      display: 'flex',
+      flexDirection: 'column',
+      textAlign: 'center',
+      alignItems: 'center',
+    },
+  });
+  const classes = useStyles();
+
+  //---------------------------------------------------------------
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [user, setUser] = useState({
+    email: '',
+    userName: '',
+    password: '',
+  });
+  const [passVisibility, setPassVisibility] = useState(false);
+  const [invalidFields, setInvalidFields] = useState({
+    wrongPassword: false,
+    inexistentUser: false,
+  });
+
+  const inputHandler = ({ target: { id, value } }) => {
+    setUser((prevState) => ({
+      ...prevState,
+      [id]: value,
+    }));
+  };
+
+  const logButton = () => {
+    const { email, password } = user;
+    const userAccount = JSON.parse(localStorage.getItem(email));
+    
+    if (userAccount
+      && password === userAccount.password) {
+      dispatch(loginAction(user));
+      navigate('/');
+    } else if (!userAccount) {
+      setInvalidFields({ wrongPassword: false, inexistentUser: true });
+      setTimeout(() =>
+        setInvalidFields((prevState) => ({ ...prevState, inexistentUser: false })), 5000);
+    } else if (password !== userAccount.password) {
+      setInvalidFields({ inexistentUser: false, wrongPassword: true });
+      setTimeout(() =>
+        setInvalidFields((prevState) => ({ ...prevState, wrongPassword: false })), 5000);
+    }
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container
+        className={classes.container}
+        maxWidth="xl"
+      >
+        <Typography
+          variant="h2"
+          className={classes.title}
+        >Log-In</Typography>
+        <Container
+          className={classes.inputSection}
+          disableGutters
+        >
+          <TextField
+            label="Email"
+            autoComplete="off"
+            placeholder="yourEmail@hotmail.com"
+            type="email"
+            className={classes.input}
+            InputProps={{
+              className: classes.input}}
+            id="email"
+            data-testid="email-input"
+            onChange={ inputHandler }
+          />
+        </Container>
+        <Container
+          className={classes.inputSection}
+          disableGutters
+        >
+          <TextField
+            label="password"
+            className={classes.input}
+            InputProps={{
+              className: classes.input}}
+            id="password"
+            data-testid="password-input"
+            type={
+              passVisibility
+                ? 'text'
+                : 'password'
+            }
+            onChange={ inputHandler }
+          />
+          <input
+            type="checkbox"
+            className="password-visibility"
+            onClick={
+              () => setPassVisibility((prevState) => !prevState) } />
+        </Container>
+        {invalidFields.inexistentUser
+            && <p style={ { color: 'red' } }>
+                There is nobody with this email in our database
+            </p>}
+        {invalidFields.wrongPassword
+            && <p style={ { color: 'red' } }>
+                Wrong password
+            </p>}
+        <Container className={classes.buttonSection}>
+          <Button
+            style={{ marginTop: '100px' }}
+            size="large"
+            color="primary"
+            variant="contained"
+            onClick={ logButton }
+            type="button"
+            className={classes.buttons}
+            disabled={
+              !(user.email.includes('@')
+                && user.email.includes('.com')
+                && user.password.length >= MIN_PASS_LENGTH)
+            }
+          >Log In</Button>
+          <Button
+            size="large"
+            onClick={ () => navigate('/signin') }
+            className={classes.buttons}
+            color="primary"
+            variant="outlined"
+          >
+            Create account
+          </Button>
+          <Button
+            className={classes.buttons}
+            size="small"
+            style={{
+              fontSize: '70%',
+              padding: '5px',
+              textDecoration: 'underline',
+              color: '#00ADB5'
+            }}
+          >
+            I forgot my password
+          </Button>
+        </Container>
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default Login;
